d358b40dbdb79d11a363820893623b7f97a9f240
said1854 <msaliogullari@gmail.com>
2022-10-03 14:43:56 +0300

    apikey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Card from './components/Card';
 
 function App() {
   return (<>
     <h1>App</h1>
+    <Card />
     <p> Simple paragraph</p>
     </>
   )
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+        <h1>Movie Name</h1>
+        <p>description</p>
+        <p>info</p>
+    </div>
+  )
+}
+
+export default Card;
