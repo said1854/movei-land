1d9c0e11c89b181fc3f7a18136bb6b3df37090a7
said1854 <msaliogullari@gmail.com>
2022-10-05 14:44:41 +0300

    css added

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@ import React, {useEffect} from 'react'
 import Card from './components/Card';
 
 const apikey = '3399fc3f';
-console.log(apikey)
+const url = `http://www.omdbapi.com/?apikey=${apikey}`
 
-const url = `https://www.omdbapi.com/?apikey=${apikey}`
-const App = () => {
-  
+const App = () => {  
   const searchMovies = async (title)=>{
     const response = await fetch(`${url}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search[3]);
+    console.log(data);
   }
-  
-
 
   useEffect(()=>{
-    searchMovies('wanted')
+    searchMovies('spiderman')
   }, []);
   
   return (<>
     <h1>App</h1>
     <Card />
     <p> Simple paragraph</p>
-    </>
-  )
+    </>)
 }
 
 export default App;
